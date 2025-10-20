0e60d0baee6a8fbfeaffbd48c534a1a2314bdad2
Juno Moneta <dev@junomoneta.io>
2025-10-20 14:47:57 +0000

    Update Debian package build script for rebranding
    - Rename package from zcash to junocash
    - Update all binary paths (zcashd → junocashd, etc.)
    - Update man page references
    - Update bash completion file references
    - Rename and update junocash-fetch-params man page

--- zcutil/build-debian-package.sh
+++ zcutil/build-debian-package.sh
@@ -6,8 +6,8 @@ export LC_ALL=C
 set -e
 set -x
 
-BUILD_PATH="/tmp/zcbuild"
-PACKAGE_NAME="zcash"
+BUILD_PATH="/tmp/junocashbuild"
+PACKAGE_NAME="junocash"
 SRC_PATH=$(pwd)
 SRC_DEB=$SRC_PATH/contrib/debian
 SRC_DOC=$SRC_PATH/doc
@@ -18,7 +18,7 @@ if [ ! -d $BUILD_PATH ]; then
     mkdir $BUILD_PATH
 fi
 
-PACKAGE_VERSION=$($SRC_PATH/src/zcashd --version | grep version | cut -d' ' -f4 | tr -d v)
+PACKAGE_VERSION=$($SRC_PATH/src/junocashd --version | grep version | cut -d' ' -f4 | tr -d v)
 DEBVERSION=$(echo $PACKAGE_VERSION | sed 's/-beta/~beta/' | sed 's/-rc/~rc/' | sed 's/-/+/')
 BUILD_DIR="$BUILD_PATH/$PACKAGE_NAME-$PACKAGE_VERSION-amd64"
 
@@ -39,35 +39,35 @@ chmod 0755 -R $BUILD_DIR/*
 #cp $SRC_DEB/preinst $BUILD_DIR/DEBIAN
 #cp $SRC_DEB/prerm $BUILD_DIR/DEBIAN
 # Copy binaries
-cp $SRC_PATH/src/zcashd $DEB_BIN
-cp $SRC_PATH/src/zcashd-wallet-tool $DEB_BIN
-cp $SRC_PATH/src/zcash-cli $DEB_BIN
-cp $SRC_PATH/zcutil/fetch-params.sh $DEB_BIN/zcash-fetch-params
+cp $SRC_PATH/src/junocashd $DEB_BIN
+cp $SRC_PATH/src/junocashd-wallet-tool $DEB_BIN
+cp $SRC_PATH/src/junocash-cli $DEB_BIN
+cp $SRC_PATH/zcutil/fetch-params.sh $DEB_BIN/junocash-fetch-params
 # Copy docs
 cp $SRC_PATH/doc/release-notes/release-notes-1.0.0.md $DEB_DOC/changelog
 cp $SRC_DEB/changelog $DEB_DOC/changelog.Debian
 cp $SRC_DEB/copyright $DEB_DOC
 cp -r $SRC_DEB/examples $DEB_DOC
 # Copy manpages
-cp $SRC_DOC/man/zcashd.1 $DEB_MAN
-cp $SRC_DOC/man/zcashd-wallet-tool.1 $DEB_MAN
-cp $SRC_DOC/man/zcash-cli.1 $DEB_MAN
-cp $SRC_DOC/man/zcash-fetch-params.1 $DEB_MAN
+cp $SRC_DOC/man/junocashd.1 $DEB_MAN
+cp $SRC_DOC/man/junocashd-wallet-tool.1 $DEB_MAN
+cp $SRC_DOC/man/junocash-cli.1 $DEB_MAN
+cp $SRC_DOC/man/junocash-fetch-params.1 $DEB_MAN
 # Copy bash completion files
-cp $SRC_PATH/contrib/zcashd.bash-completion $DEB_CMP/zcashd
-cp $SRC_PATH/contrib/zcash-cli.bash-completion $DEB_CMP/zcash-cli
+cp $SRC_PATH/contrib/junocashd.bash-completion $DEB_CMP/junocashd
+cp $SRC_PATH/contrib/junocash-cli.bash-completion $DEB_CMP/junocash-cli
 # Gzip files
 gzip --best -n $DEB_DOC/changelog
 gzip --best -n $DEB_DOC/changelog.Debian
-gzip --best -n $DEB_MAN/zcashd.1
-gzip --best -n $DEB_MAN/zcashd-wallet-tool.1
-gzip --best -n $DEB_MAN/zcash-cli.1
-gzip --best -n $DEB_MAN/zcash-fetch-params.1
+gzip --best -n $DEB_MAN/junocashd.1
+gzip --best -n $DEB_MAN/junocashd-wallet-tool.1
+gzip --best -n $DEB_MAN/junocash-cli.1
+gzip --best -n $DEB_MAN/junocash-fetch-params.1
 
 cd $SRC_PATH/contrib
 
 # Create the control file
-dpkg-shlibdeps $DEB_BIN/zcashd $DEB_BIN/zcash-cli
+dpkg-shlibdeps $DEB_BIN/junocashd $DEB_BIN/junocash-cli
 dpkg-gencontrol -P$BUILD_DIR -v$DEBVERSION
 
 # Create the Debian package
