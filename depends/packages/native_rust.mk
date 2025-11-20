95c48af8c01788be7607446dd65506bead574c6c
Juno Moneta <dev@junomoneta.io>
2025-11-20 04:06:10 +0000

    Fix ARM64 macOS Rust and CRC32C detection
    On Apple Silicon, CRC32 instructions are always available, so skip
    getauxval check which doesn't exist on macOS. Adds __APPLE__ check
    to return true directly for macOS ARM64.
    
    - Add aarch64-apple-darwin Rust 1.81.0 download support to depends
    - Fix rust_target detection for ARM64 macOS hosts
    - Fix CRC32C to skip getauxval on macOS (always available on Apple Silicon)
    
    Enables native ARM64 builds on Apple Silicon Macs.

--- depends/packages/native_rust.mk
+++ depends/packages/native_rust.mk
@@ -9,17 +9,16 @@ $(package)_file_name_linux=rust-$($(package)_version)-x86_64-unknown-linux-gnu.t
 $(package)_sha256_hash_linux=4ca7c24e573dae2f382d8d266babfddc307155e1a0a4025f3bc11db58a6cab3e
 $(package)_file_name_darwin=rust-$($(package)_version)-x86_64-apple-darwin.tar.gz
 $(package)_sha256_hash_darwin=f74d8ad24cc3cbfb825da98a08d98319565e4d18ec2c3e9503bf0a33c81ba767
+$(package)_file_name_aarch64_darwin=rust-$($(package)_version)-aarch64-apple-darwin.tar.gz
+$(package)_sha256_hash_aarch64_darwin=60a41dea4ae0f4006325745a6400e6fdc3e08ad3f924fac06f04c238cf23f4ec
 $(package)_file_name_freebsd=rust-$($(package)_version)-x86_64-unknown-freebsd.tar.gz
 $(package)_sha256_hash_freebsd=b96ebbc043058eedebccd20f1d01e64f2241107665fe2336e6927966d8b9d8d3
 $(package)_file_name_aarch64_linux=rust-$($(package)_version)-aarch64-unknown-linux-gnu.tar.gz
 $(package)_sha256_hash_aarch64_linux=ef4da9c1ecd56bbbb36f42793524cce3062e6a823ae22cb679a945c075c7755b
 
 # Mapping from GCC canonical hosts to Rust targets
-# If a mapping is not present, we assume they are identical, unless $host_os is
-# "darwin", in which case we assume x86_64-apple-darwin.
 $(package)_rust_target_x86_64-pc-linux-gnu=x86_64-unknown-linux-gnu
 $(package)_rust_target_x86_64-w64-mingw32=x86_64-pc-windows-gnu
-$(package)_rust_target_aarch64-apple-darwin=aarch64-apple-darwin
 
 # Mapping from Rust targets to SHA-256 hashes
 $(package)_rust_std_sha256_hash_aarch64-unknown-linux-gnu=234673e33b7a523818a81dc233ba636ffc5e4c94b9766f12e19a63c985ed7d21
@@ -29,7 +28,7 @@ $(package)_rust_std_sha256_hash_x86_64-pc-windows-gnu=08fdb3e632bd0571e2a41f7281
 $(package)_rust_std_sha256_hash_x86_64-unknown-freebsd=9dbff8d29bd32bab0c68a2cda0fb38059cca6fbd962a8f243400388c104cb225
 
 define rust_target
-$(if $($(1)_rust_target_$(2)),$($(1)_rust_target_$(2)),$(if $(findstring darwin,$(3)),x86_64-apple-darwin,$(if $(findstring freebsd,$(3)),x86_64-unknown-freebsd,$(2))))
+$(if $($(1)_rust_target_$(2)),$($(1)_rust_target_$(2)),$(if $(findstring darwin,$(3)),aarch64-apple-darwin,$(if $(findstring freebsd,$(3)),x86_64-unknown-freebsd,$(2))))
 endef
 
 define $(package)_set_vars
